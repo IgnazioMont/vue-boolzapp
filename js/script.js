48341090b810dd3b80afed63d3236c995d8d065f
Ignazio Montebello <ignaziom86@yahoo.it>
2021-04-23 16:03:07 +0200

    Input + data

--- js/script.js
+++ js/script.js
@@ -9,6 +9,22 @@
 		- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare 
 			tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione;
 		- Click sul contatto mostra la conversazione del contatto cliccato.
+
+	Milestone 3
+		- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” 
+			il testo viene aggiunto al thread sopra, come messaggio verde;
+		- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà 
+			un “ok” come risposta, che apparirà dopo 1 secondo.
+
+	Milestone 4
+		- Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo 
+			i contatti il cui nome contiene le lettere inserite 
+			(es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina).
+	
+	Milestone 5 (opzionale)
+		- Cancella messaggio: cliccando sul messaggio appare un menu a tendina 
+			che permette di cancellare il messaggio selezionato;
+		- Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
 */
 
 const boolZapp = new Vue({
@@ -140,6 +156,25 @@ const boolZapp = new Vue({
 					element.visible = false;
 				}
 			});
+		},
+
+		//	Messaggio inviato al click-enter rilasciato
+		sendMessage() {
+			if (this.userInput != "") {
+				this.contacts[this.contactIndex].messages.push(
+					{
+						visibility: "hidden",
+						date: dayjs().format("DD/MM/YYYY HH:mm:ss"),
+						text: this.userInput,
+						status: 'sent'
+					});
+				
+				/* this.recivedMessage(); */
+			}
+		},
+
+		receivedMessage() {
+
 		}
 	}
 });
